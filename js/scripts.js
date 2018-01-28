34948b63da762c7dee0cb6f5a6bb9632e73babb0
Panatda Inthala <panatdainthala@gmail.com>
2018-01-27 21:47:35 -0800

    it will have complete js with business and front logic, determining cost formula, object constructor, creates prototype to run function with if statments, add reset feature and text in results with cost.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  // Returns the value of the pizza size radio group
   var getSize = function() {
     var size = $('input[name="pizza-size"]:checked').val();
     console.log(size)
     return size;
   }
+
+  // Returns the value of the pizza toppings
   var getTopping = function() {
     var topping = $('input[name="toppings"]:checked').map(function(index, toppingElement) {
       return $(toppingElement).val();
@@ -14,11 +17,63 @@ $(document).ready(function() {
     console.log(topping)
     return (topping);
   }
+
+//object constructor funtion
+  function Pizza(topping, size) {
+    this.topping = topping;
+    this.size = size;
+  }
+
+//create pizzaCostrunning funtion with if statments
+  Pizza.prototype.pizzaCost = function() {
+    var cost = 5;
+    if (this.size == 'medium') {
+      cost += 2;
+    } else if (this.size == 'large') {
+      cost += 4;
+    }
+
+
+    if (this.topping.length == 2) {
+      cost += 2;
+
+    } else if (this.topping.length == 3) {
+      cost += 4;
+    }
+    return cost;
+  }
+
+//reset val to true of false when the reset is submit
+  var reset = function() {
+    $('input[value="small"]').prop('checked',true);
+    $('input[value="stars"]').prop('checked',false);
+    $('input[value="flowers"]').prop('checked',false);
+    $('input[value="mushrooms"]').prop('checked',false);
+
+
+  //  var pizzaTopping = $("#pizzaTopping").val('');
+    $("#show-price").text('')
+    $("#reset").hide();
+  };
+
   $("#pizza-form").submit(function(event) {
     event.preventDefault();
     //create new vars
     var pizzaSize = getSize(); //functions will be ran there
     var pizzaTopping = getTopping();
-
+    //pizza function returns object, made contructor
+    var customPizza = new Pizza(pizzaTopping, pizzaSize);
+    //call method of pizza object
+    var pizzaCost = customPizza.pizzaCost();
+    //display results
+    $('#show-price').text("Your pizza cost is " + "$" + pizzaCost);
+    $('#show-price').show();
+    $("#reset").show();
+    //found on stack overflow helps scrolls to the end of page to better show results
+    $(window).scrollTop($(document).height());
   });
+
+  $('#reset').hide();
+  $("#reset").click(reset);
+
 });
